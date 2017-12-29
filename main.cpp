51a600bcade7849ba34653367b9bd5395b27b8da
jptboy <34559951+jptboy@users.noreply.github.com>
2017-12-29 01:42:23 -0700

    first edits for calculator

--- main.cpp
+++ main.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
+using namespace std;
+int addition(int x, int y);
+//int subtraction();
+//double multiplication();
+//double division();
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+int main()
+{
+    int x;
+    int y;
+    cout<< " Enter in the two numbers you wish to add." << endl;
+    cin >> x;
+    cin >> y;
+    int z;
+    z = addition(x, y);
+    cout << z;
+
+
+
 }
+
+
+int addition(int x, int y)
+{
+    int meme = x;
+    int beme = y;
+    int z;
+    z = meme+beme;
+    return z;
+}
+
